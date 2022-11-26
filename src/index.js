dbb6a7eb0e5cb5ed585c4b04c9a3060029e50b39
jcala09 <b.cala@ttu.edu>
2022-11-26 12:40:11 -0600

    reverted index back &set base directory to build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'))
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
